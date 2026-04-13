237b9499396da9fd6a38465c8d35ea070a3bae1d
rufaidaalrahbi <rufaidaalrahbi292@gmail.com>
2026-04-13 21:37:27 +0400

    Update driverRoutes.js

--- travelbuddy-server/routes/driverRoutes.js
+++ travelbuddy-server/routes/driverRoutes.js
@@ -1,14 +1,16 @@
 import express from "express";
+//import { getAllDrivers } from "../controllers/driverController.js";
 import {
   driverRegister,
   driverLogin,
 verifyDriverByAdmin,
-} from "../controllers/driverController_fixed.js";
+} from "../controllers/driverController.js";
 
 const router = express.Router();
 
 router.post("/driverRegister", driverRegister);
 router.post("/driverLogin", driverLogin);
+//router.get("/drivers" , getAllDrivers);
 
 // admin route
 router.patch("/verifyDriver/:driverId", verifyDriverByAdmin);
